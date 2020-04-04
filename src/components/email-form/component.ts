0a9fff8d11753d034c12b06038786bbafbdb42d5
Marlos Borges <marlos.borges@gmail.com>
2020-04-04 22:06:41 +0200

    fix form rendering of emails input

--- src/components/email-form/component.ts
+++ src/components/email-form/component.ts
@@ -1,9 +1,10 @@
 import './main.sass'
 import template from './template.html'
-import { createElement } from 'utils/dom'
+import { createElement, q } from 'utils/dom'
 import { EmailsInput } from 'components/emails-input'
 
 export class EmailForm {
+  private body: HTMLElement
 
   constructor (
     private readonly container: HTMLElement,
@@ -14,9 +15,10 @@ export class EmailForm {
 
   private render (): void {
     const form = createElement('forn', template, ['email-form'])
+    this.body = q(form, '.email-form-header__body')
 
     for (let i = 0; i++ < this.inputs;) {
-      new EmailsInput(form)
+      new EmailsInput(this.body)
     }
 
     this.container.prepend(form)
