467cf55027a92b6f7bded58c1706483472a96e05
Marlos Borges <marlos.borges@gmail.com>
2020-04-04 19:39:17 +0200

    add dom utils

---
+++ src/utils/dom.ts
@@ -0,0 +1,24 @@
+/**
+ * Creates an element and adds optional classes
+ */
+export const createElement = (tagName: string, ...classes: string[]): HTMLElement => {
+  const el = document.createElement(tagName)
+  el.classList.add(...classes)
+
+  return el
+}
+
+/**
+ * Creates a text node
+ */
+export const createTextNode = (text: string): Text => document.createTextNode(text)
+
+/**
+ * An alias for querySelector with optional context node
+ */
+export const q = <E extends Element>(context: string | ParentNode, selector: string): E | null => {
+  if (typeof context === 'string') {
+    return document.querySelector(context)
+  }
+  return context.querySelector(selector)
+}
