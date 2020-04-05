ab33f8326f6370e73f08ec6d5541c9b6f3729625
Marlos Borges <marlos.borges@gmail.com>
2020-04-05 13:53:28 +0200

    add event to remove email on clicking close button

--- src/components/email-chip/email-chip.ts
+++ src/components/email-chip/email-chip.ts
@@ -22,9 +22,16 @@ export class EmailChip {
     return this.isValid ? classes : classes.concat('email-chip--invalid')
   }
 
+  private addCloseListener(): void {
+    q(this._element, '.email-chip__close-button').addEventListener('click', () => {
+      this._element.remove()
+    })
+  }
+
   private createElement (): void {
     this._element = createElement('div', template, this.styleClasses)
 
     q(this._element, '.email-chip__address').innerHTML = this.emailAddress
+    this.addCloseListener()
   }
 }
